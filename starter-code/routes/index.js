cf5c1e4386b7f66f576d5726decc24afd19f1545
Gooss <gooss@media-saturn.com>
2020-01-07 15:00:10 +0100

    Iteration 2 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/celebrity');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+    .then(allCelebsFromDB => {
+      res.render('celebrities', { celebrities: allCelebsFromDB });
+    })
+    .catch(error => {
+      console.log('Error while getting the records from the DB: ', error);
+    })
+});
+
+
 
 module.exports = router;
