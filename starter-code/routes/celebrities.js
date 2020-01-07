3226379062e7fb23d7ca4d59881f1fc37202c666
Rochowski <rochowskim@media-saturn.com>
2020-01-07 16:14:26 +0100

    iteration 3 completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -7,11 +7,23 @@ router.get('/', (req, res, next) => {
     Celebrity.find()
       .then(allTheCelebritiesFromDB => {
         // console.log('Retrieved celebtrites from DB:', allTheCelebritiesFromDB);
+        //console.log(allTheCelebritiesFromDB);
         res.render('celebrities/index', { celebrities: allTheCelebritiesFromDB });
       })
       .catch(error => {
         console.log('Error while getting the celebrities from the DB: ', error);
       });
+  });
+
+  router.get('/:celebritiesId', (req, res, next) => {
+      //console.log('this is my request log ' + req.params.celebritiesId);
+    Celebrity.findById(req.params.celebritiesId)
+      .then(theCelebrity => {
+        res.render('celebrities/show', { celebrity: theCelebrity });
+      })
+      .catch(error => {
+        console.log('Error while retrieving celebrity details: ', error);
+      })
   });
 
   module.exports = router;
