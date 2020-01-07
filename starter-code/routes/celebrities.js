d7beff8a7fc564cd34d54857daa17ced07fe52fe
Malte Schenkling <schenkling@mediamarktsaturn.com>
2020-01-07 16:31:21 +0100

    Iteration 6 upgr to separate extra route file

---
+++ starter-code/routes/celebrities.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router  = express.Router();
+const Celebrity = require('../models/celebrity.js');
+
+/* GET Celebrities */
+router.get('/', (req, res, next) => {
+    Celebrity.find()
+    .then(allCelebsFromDB => {
+      console.log({celeb: allCelebsFromDB});
+      res.render('celebrities/index.hbs', {celeb: allCelebsFromDB});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+  
+  router.post('/', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+    newCelebrity.save()
+    .then(newCeleb => {
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('celebrities/new.hbs');
+    });
+  });
+  
+  router.get('/new', (req, res, next) => {
+      res.render('celebrities/new.hbs');
+  });
+  
+  router.get('/:id/delete', (req, res, next) => {
+    Celebrity.findByIdAndRemove({ '_id': req.params.id })
+    .then(oneCelebFromDB => {
+      console.log({celeb: oneCelebFromDB});
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+  
+  router.get('/:id/edit', (req, res, next) => {
+    Celebrity.findOne({ '_id': req.params.id })
+    .then(oneCelebFromDB => {
+      console.log({celeb: oneCelebFromDB});
+      res.render('celebrities/edit', {celeb: oneCelebFromDB});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+  
+  router.post('/:id', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    Celebrity.update({_id: req.params.id}, {$set: { name, occupation, catchPhrase }})
+    .then(newCeleb => {
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  })
+  
+  router.get('/:id', (req, res, next) => {
+    Celebrity.findOne({ '_id': req.params.id })
+    .then(oneCelebFromDB => {
+      console.log({celeb: oneCelebFromDB});
+      res.render('celebrities/show.hbs', {celeb: oneCelebFromDB});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+
+  module.exports = router;
