b646c9978ce8d127819af921bcf8674bf2757c4a
Rochowski <rochowskim@media-saturn.com>
2020-01-08 13:45:29 +0100

    iteration 6 completed

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,55 +1,79 @@
-const express = require('express');
-const router  = express.Router();
-const Celebrity = require('../models/celebrity');
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/celebrity");
 
+router.get("/new", (req, res, next) => {
+  res.render("celebrities/new");
+});
 
-router.get('/', (req, res, next) => {
-    Celebrity.find()
-      .then(allTheCelebritiesFromDB => {
-        // console.log('Retrieved celebtrites from DB:', allTheCelebritiesFromDB);
-        //console.log(allTheCelebritiesFromDB);
-        res.render('celebrities/index', { celebrities: allTheCelebritiesFromDB });
-      })
-      .catch(error => {
-        console.log('Error while getting the celebrities from the DB: ', error);
-      });
-  });
-
-  router.get('/new', (req, res, next) => {
-    res.render("celebrities/new");
-  });
-
-  router.post('/new', (req, res, next) => {
-    const newCelebrity = new Celebrity(req.body);
-    newCelebrity.save()
-      .then((celebrity) => {
-        res.redirect('/celebrities');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-
-  router.get('/:celebritiesId', (req, res, next) => {
-      //console.log('this is my request log ' + req.params.celebritiesId);
-    Celebrity.findById(req.params.celebritiesId)
-      .then(theCelebrity => {
-        res.render('celebrities/show', { celebrity: theCelebrity });
-      })
-      .catch(error => {
-        console.log('Error while retrieving celebrity details: ', error);
-      })
-  });
-
-  router.post('/:celebritiesId/delete', (req, res, next) => {
-    //console.log('this is my request log ' + req.params.celebritiesId);
-  Celebrity.findByIdAndRemove(req.params.celebritiesId)
-  .then((celebrity) => {
-    res.redirect('/celebrities');
-  })
+router.post("/new", (req, res, next) => {
+  const newCelebrity = new Celebrity(req.body);
+  newCelebrity
+    .save()
+    .then(celebrity => {
+      res.redirect("/celebrities");
+    })
     .catch(error => {
-      console.log('Error while delete celebrity: ', error);
+      next(error);
     });
 });
 
-  module.exports = router;
+router.get("/:celebritiesId/edit", (req, res, next) => {
+  Celebrity.findById(req.params.celebritiesId)
+    .then(theCelebrity => {
+      res.render("celebrities/edit", { celebrity: theCelebrity });
+    })
+    .catch(error => {
+      console.log("Error while retrieving celebrity details: ", error);
+    });
+});
+
+router.post("/edit", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.update(
+    { _id: req.params.celebritiesId },
+    { $set: { name, occupation, catchPhrase } }
+  )
+    .then(celebrity => {
+      res.redirect("/celebrities");
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.post("/:celebritiesId/delete", (req, res, next) => {
+  //console.log('this is my request log ' + req.params.celebritiesId);
+  Celebrity.findByIdAndRemove(req.params.celebritiesId)
+    .then(celebrity => {
+      res.redirect("/celebrities");
+    })
+    .catch(error => {
+      console.log("Error while delete celebrity: ", error);
+    });
+});
+
+router.get("/:celebritiesId", (req, res, next) => {
+  //console.log('this is my request log ' + req.params.celebritiesId);
+  Celebrity.findById(req.params.celebritiesId)
+    .then(theCelebrity => {
+      res.render("celebrities/show", { celebrity: theCelebrity });
+    })
+    .catch(error => {
+      console.log("Error while retrieving celebrity details: ", error);
+    });
+});
+
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then(allTheCelebritiesFromDB => {
+      // console.log('Retrieved celebtrites from DB:', allTheCelebritiesFromDB);
+      //console.log(allTheCelebritiesFromDB);
+      res.render("celebrities/index", { celebrities: allTheCelebritiesFromDB });
+    })
+    .catch(error => {
+      console.log("Error while getting the celebrities from the DB: ", error);
+    });
+});
+
+module.exports = router;
