07b5c4fbf6e8345c226bbaf23891b0022a622f75
Chantel Taki <chanteltaki@gmail.com>
2020-04-23 11:29:19 -0400

    iteration 5

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -7,7 +7,7 @@ const Celebrity = require('../models/Celebrity');
 router.get('/', (req, res) => {
   // get all the celebs
   Celebrity.find().then(celebritiesList => {
-    // render a 'celeb' view with the data
+    // render 'celebs' view with the data
     res.render('celebrities/index', {celebrities: celebritiesList,});
   }).catch((error) => {
     console.log(error);
@@ -15,7 +15,9 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res, next) => {
+  //get individual celeb
     Celebrity.findById(req.params.id).then(celebrity => {
+      //render each celeb view with the data
         res.render('celebrities/show', {celebrity: celebrity});
     }).catch((error) => {
     console.log(error);
@@ -24,16 +26,19 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.get('/new', (req, res) => {
+  //create new celeb page
   res.render('celebrities/new');
 });
 
 router.post('/', (req, res) => {
+  //post form for new celeb
   const { name, occupation, catchPhrase} = req.body;
   Celebrity.create({
     name: name,
     occupation: occupation,
     catchPhrase: catchPhrase
   }).then(celebrity => {
+    //render view for new celeb
     console.log(`Success ${celebrity} was added to the database`);
     res.redirect(`/celebrities`);
   }).catch(err => {
@@ -41,5 +46,15 @@ router.post('/', (req, res) => {
     // logs the error to the console
     next(err);
   });
+});
+
+router.post('/:id/delete', (req, res) => {
+  Celebrity.findByIdAndRemove({ _id: req.params.id })
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 module.exports = router;
