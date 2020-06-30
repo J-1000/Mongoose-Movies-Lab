f0215401e28fa2b5e2c72b5d1e7a67a4d2a27301
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2020-06-30 10:52:42 +0200

    iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const Celebrity = require('../models/Celebrity');
-const router  = express.Router();
-// Call the Celebrity model's find method to retrieve all the celebrities.
-// If there's an error, call the route's next function and return the error.
-// If there isn't an error, render the celebrities/index view.
-// Pass the array of celebrities into the view as a variable.
-/* GET home page */
-
-// router.get('/:movieId', (req, res) => {
-//     const movieId = req.params.movieId;
-//     Movie.findById(movieId).then(movie => {
-//       res.render('movieInfo', { movie: movie });
-//     }).catch(err => {
-//       console.log(err);
-//     });
-//   });
+const router = express.Router();
 
 router.get('/', (req, res) => {
     Celebrity.find().then(celeb => {
         res.render('celebrities/index', { celeb: celeb });
     }).catch(err => {
         console.log(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    const celebId = req.params.id;
+    Celebrity.findById(celebId).then(celeb => {
+        res.render('celebrities/show', { celeb: celeb });
+    }).catch(err => {
+        console.log(err);
+        next(err);
     });
 });
 
