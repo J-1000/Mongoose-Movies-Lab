712e89c996da5d8a00d9dba98018cd8471aff3d6
vmayrock <verena.mayrock@gmail.com>
2020-06-30 15:37:31 +0200

    iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -6,13 +6,26 @@ const router=express.Router();
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
   .then((celebsFromDatabase) => {
-    console.log("hi")
+    console.log(celebsFromDatabase)
     res.render('celebrities', {celebList: celebsFromDatabase});
   }) 
   .catch(err => {
     console.log(err); 
   })
 }); 
+
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const celebId=req.params.id; 
+  Celebrity.findById(celebId)
+  .then((celebsFromDatabase) => {
+    res.render('show', {celeb: celebsFromDatabase});
+  })
+  .catch(err => {
+    console.log(err); 
+  })
+}); 
+
 
 
 module.exports=router; 
