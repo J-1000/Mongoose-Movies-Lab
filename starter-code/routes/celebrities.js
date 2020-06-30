3e1cb0bf70b70e885d830cdb31aa3e9b6d707714
nina <ninasmolnikova@gmail.com>
2020-06-30 16:06:57 +0200

    it 3,4 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -21,11 +21,48 @@ router.get('/celebrities', (req, res, next) => {
 
   
 router.get('/celebrities/:id', (req, res, next) => {
+    
     Celebrity.findById(req.params.id).then(celebrity => {
-        res.render('celebrity-details.hbs', {celebrity: celebrity})
+        res.render('celebrities/show.hbs', {celebrity: celebrity})
     }).catch(error => {
         console.log('Error', error);
       })
 });
+
+
+router.get('/add', (req, res, next) => {
+    res.render('celebrities/new');
+
+});
+
+
+
+// router.post('/celebrity', (req, res) => {
+//     console.log(req.body); 
+//     const { name, occupation, catchPhrase } = req.body;
+//     Celebrity.create({
+//       name: name,
+//       occupation: occupation,
+//       catchPhrase: catchPhrase
+//     }).then(celebrity => {
+//       console.log(`Success! ${name} was added to the database.`);
+//       res.redirect(`/books/${celebrity._id}`);
+//     }).catch(err => {
+//       console.log(err);
+//     })
+//   })
   
+  
+router.post('/celebrities', (req, res, next) => {
+    const {  name, occupation, catchPhrase } = req.body;
+    const newCelebrity = new Celebrity({  name, occupation, catchPhrase})
+    newCelebrity.save()
+    .then((celebrity) => {
+      res.redirect('/celebrities');
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  });
+
 module.exports = router;
