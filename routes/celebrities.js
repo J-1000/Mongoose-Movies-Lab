945745d62764145e13de245b37a2f7b7a5fd2692
Marco Lölkes <marcoloelkes@gmx.de>
2022-04-18 18:11:41 +0200

    implement CRUD logic

---
+++ routes/celebrities.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const Celebrity = require('../models/Celebrity');
+
+router.get('/celebrities', (req, res, next) => {
+    // get all celebrities
+    Celebrity.find()
+    .then(celebrities => {
+        res.render('celebrities/index', {celebrities: celebrities})
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/celebrities/new', (req, res, next) => {
+
+    res.render('celebrities/new');
+
+})
+
+router.post('/celebrities', (req, res, next) => {
+
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity.create({
+        name: name,
+        occupation: occupation,
+        catchPhrase: catchPhrase
+    })
+    .then(createdCelebrity => {
+        res.redirect('celebrities');
+    })
+    .catch(err => {
+        res.render('celebrities/new');
+    })
+
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+    const id = req.params.id;
+    Celebrity.findById(id)
+    .then(celebrity => {
+        res.render('celebrities/show', {celebrity: celebrity})
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+    const id = req.params.id;
+    Celebrity.findByIdAndRemove(id)
+    .then(query => {
+        res.redirect('/celebrities')
+    })
+    
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+    const id = req.params.id;
+    Celebrity.findById(id)
+    .then(celebrity => {
+        res.render('celebrities/edit', {celebrity: celebrity})
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.post('/celebrities/:id', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body
+
+    const celebrity = {
+        name: name,
+        occupation: occupation,
+        catchPhrase: catchPhrase
+    }
+
+    Celebrity.update(req.body._id, celebrity)
+    .then(createdCelebrity => {
+        res.redirect('/celebrities');
+    })
+    .catch(err => {
+        res.render('celebrities/new');
+    })
+
+})
+
+module.exports = router;
